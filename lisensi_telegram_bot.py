2cb2184c30b325602463bd8aefab5b1b671d38d0
Reza Adil Hakim <43247492+rezadkim@users.noreply.github.com>
2025-03-30 16:36:47 +0700

    Update lisensi_telegram_bot.py

--- lisensi_telegram_bot.py
+++ lisensi_telegram_bot.py
@@ -7,10 +7,10 @@ from telegram.ext import (
 from datetime import datetime
 
 # Konfigurasi bot
-TOKEN = '8130489744:AAF6c46zwzEDN8pvhdFbKnlI9CO7mds3swk'
+TOKEN = 'Token BOT'
 BASE_URL = 'http://localhost/lisensimanager/api'
 ADMIN_TOKEN = '2e52971b72004c2555b8b59dda0108ae9e157a5d17cbe434a457cffd55c42e3c'
-ADMIN_ID = 6994514050
+ADMIN_ID = 'userid telegram anda (integer)'
 
 # State conversation
 (
